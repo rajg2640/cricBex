0b28b9d587eb3988747557f896b97fd7ca06ccf6
Akash Tagline <akm@taglineinfotech.com>
2025-11-12 12:39:57 +0530

    chore: rewrite backend endpoint to our server

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://94.136.188.185:5001/api/:path*",
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
--- src/services/sportbex.js
+++ src/services/sportbex.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const sportbexClient = axios.create({
-  baseURL: "http://94.136.188.185:5001/api/cricbex",
+  baseURL: "/api/cricbex",
   headers: {
     "sb-live-score-api-key": process.env.NEXT_PUBLIC_SPORTBEX_API_KEY,
   },
