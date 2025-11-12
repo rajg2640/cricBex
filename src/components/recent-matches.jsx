a5eff515850c4190cffd96ef364ace7604df6df6
Akash Tagline <akm@taglineinfotech.com>
2025-11-12 14:37:22 +0530

    feat: refetch recent matches every 10 seconds

--- src/components/recent-matches.jsx
+++ src/components/recent-matches.jsx
@@ -6,22 +6,27 @@ import { recentMatchesQueryOptions } from "@/queries/options";
 import { useQuery } from "@tanstack/react-query";
 import { Skeleton } from "./ui/skeleton";
 import SwiperSlider from "@/app/shared/SwiperSlider";
-import { SwiperSlide } from 'swiper/react';
-
+import { SwiperSlide } from "swiper/react";
 
 export const RecentMatches = () => {
-  const { data: recentMatches, isLoading } = useQuery(
-    recentMatchesQueryOptions
-  );
-if (isLoading) {
+  const { data: recentMatches, isLoading } = useQuery({
+    ...recentMatchesQueryOptions,
+    refetchInterval: 10 * 1000, // refetch every 10 seconds
+  });
+
+  if (isLoading) {
     return (
-      <SwiperSlider spaceBetween={20} breakpoints={{
-        0: { slidesPerView: 1.25 },
-        575: { slidesPerView: 1.5 },
-        768: { slidesPerView: 2.5 },
-        1024: { slidesPerView: 3.25 },
-        1280: { slidesPerView: 4 }
-      }} className="w-full">
+      <SwiperSlider
+        spaceBetween={20}
+        breakpoints={{
+          0: { slidesPerView: 1.25 },
+          575: { slidesPerView: 1.5 },
+          768: { slidesPerView: 2.5 },
+          1024: { slidesPerView: 3.25 },
+          1280: { slidesPerView: 4 },
+        }}
+        className="w-full"
+      >
         {new Array(4).fill(0).map((_, i) => (
           <SwiperSlide key={i}>
             <Skeleton className="h-[183px] w-full rounded-2xl" />
@@ -31,24 +36,26 @@ if (isLoading) {
     );
   }
 
-  return <>
-  <SwiperSlider
-      className="w-full"
-      spaceBetween={0}
-      breakpoints={{
-        0: { slidesPerView: 1.25 },
-        575: { slidesPerView: 1.5 },
-        575: { slidesPerView: 1.5 },
-        768: { slidesPerView: 2.5 },
-        1024: { slidesPerView: 3.25 },
-        1280: { slidesPerView: 4 }
-      }}
-    >
-    {recentMatches.map((match, i) => 
-    <SwiperSlide>
-      <MatchCard key={i} data={match} />
-    </SwiperSlide>
-    )}
-  </SwiperSlider>
-  </>
+  return (
+    <>
+      <SwiperSlider
+        className="w-full"
+        spaceBetween={0}
+        breakpoints={{
+          0: { slidesPerView: 1.25 },
+          575: { slidesPerView: 1.5 },
+          575: { slidesPerView: 1.5 },
+          768: { slidesPerView: 2.5 },
+          1024: { slidesPerView: 3.25 },
+          1280: { slidesPerView: 4 },
+        }}
+      >
+        {recentMatches.map((match, i) => (
+          <SwiperSlide>
+            <MatchCard key={i} data={match} />
+          </SwiperSlide>
+        ))}
+      </SwiperSlider>
+    </>
+  );
 };
